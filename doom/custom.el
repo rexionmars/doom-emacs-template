32f3aeee936a8c3434b6545cb1c95208435d336b
rexionmars <opensource.leonardi@gmail.com>
2023-09-23 19:11:21 -0300

    doom template

---
+++ doom/custom.el
@@ -0,0 +1,16 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(ispell-dictionary nil)
+ '(magit-todos-insert-after '(bottom) nil nil "Changed by setter of obsolete option `magit-todos-insert-at'")
+ '(package-selected-packages
+   '(flycheck-google-cpplint all-the-icons-nerd-fonts helm-icons nerd-icons-ibuffer nerd-icons-dired nerd-icons melancholy-theme tree-sitter-indent cmake-mode wakatime-mode vterm-toggle tree-sitter-langs darkokai-theme))
+ '(wakatime-cli-path "~/.wakatime/wakatime-cli"))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
